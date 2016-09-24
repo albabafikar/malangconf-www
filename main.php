50e2db06310010e2bee370ffa5202022a647daca
nawa <nawa@yahoo.com>
2016-09-24 22:32:11 +0700

    Main File Handle as -> index

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Malang Conf Site Index
+ *
+ * @license MIT
+ */
+
+// Require vendor
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/Applications/Loader.php';
