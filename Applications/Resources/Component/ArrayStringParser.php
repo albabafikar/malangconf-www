20d7b284fdbde3c2fb74858281d094a672b76bff
nawa <nawa@yahoo.com>
2016-09-25 15:50:45 +0700

    Implement To ArrayAccess

--- Applications/Resources/Component/ArrayStringParser.php
+++ Applications/Resources/Component/ArrayStringParser.php
@@ -19,7 +19,7 @@ use Slim\Collection;
  * Class ArrayStringParser
  * @package MalangPhp\Site\Conf\Component
  */
-class ArrayStringParser
+class ArrayStringParser implements \ArrayAccess
 {
     use ArrayBracketResolver;
 
@@ -55,5 +55,54 @@ class ArrayStringParser
          * Create new Collector Object
          */
         return new Collection($this->getCollection());
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset An offset to check for.
+     * @return boolean true on success or false on failure.
+     */
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset The offset to retrieve.
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->fetch($offset);
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset The offset to assign the value to.
+     * @param mixed $value  The value to set.
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->collection->set($offset, $value);
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset The offset to unset.
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        $this->collection->remove($offset);
     }
 }
